2f52b0a2d520578d5ea7515b1bd6d38d9c4d1036
Livy Li <livy@fhsu.edu>
2017-03-06 14:49:30 -0600

    multi-line output

--- check_bbsis_log.py
+++ check_bbsis_log.py
@@ -64,7 +64,7 @@ def main():
         messages_length = {key:len(value) for key,value in messages.items()}
         return_message = '{OK} info, {WARNING} error, {CRITICAL} critical '.format(
             **messages_length)
-        return_message += 'out of {} lines from {}.\n '.format(counter, log_file)
+        return_message += 'out of {} lines from {}.\n'.format(counter, log_file)
         if messages['CRITICAL']:
             code = NAGIOS_LEVEL['CRITICAL']
             return_message += '\n'.join(messages['CRITICAL'][-4:])
@@ -73,7 +73,7 @@ def main():
             return_message += '\n'.join(messages['WARNING'][-4:])
         else:
             code = NAGIOS_LEVEL['OK']
-            return_message += '\n'.join(messages['OK'][-2:])
+            return_message += '\n'.join(messages['OK'][-4:])
         return_message += '|All={}'.format(counter)
         return_message += ' OK={OK} WARNING={WARNING};0;1;1;1 CRITICAL={CRITICAL};0;1;1;1'.format(
             **messages_length)
