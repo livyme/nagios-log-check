05478f46b228b69fcce2375f6265565175a28d48
Livy Li <livy@fhsu.edu>
2017-03-06 09:59:28 -0600

    data structure reformat

--- check_bbsis_log.py
+++ check_bbsis_log.py
@@ -2,9 +2,14 @@
 import sys
 import argparse
 
-NAGIOS_OK = 0
-NAGIOS_WARNING = 1
-NAGIOS_CRITICAL = 2
+NAGIOS_LEVEL = {'OK': 0,
+                'WARNING': 1,
+                'CRITICAL': 2}
+LOG_TO_NAGIOS_MAPPING = {'DEBUG': 'OK',
+                         'INFO': 'OK',
+                         'WARNING': 'WARNING',
+                         'ERROR': 'CRITICAL',
+                         'CRITICAL': 'CRITICAL'}
 
 
 def _exit(code, message):
@@ -13,28 +18,23 @@ def _exit(code, message):
 
 
 def load_log(filename):
-    message = []
-    counters = {'All': 0,
-                'DEBUG': 0,
-                'INFO': 0,
-                'WARNING': 0,
-                'ERROR': 0,
-                'CRITICAL': 0}
+    messages = {}
+    for level in NAGIOS_LEVEL.keys():
+        messages[level] = []
+    counter = 0
     with open(filename) as f:
         for line in f:
             line = line.strip()
             if line:
-                counters['All'] += 1
-                if any(log_level in line for log_level in counters.keys()):
+                counter += 1
+                if any(log_level in line for log_level in LOG_TO_NAGIOS_MAPPING.keys()):
                     line_array = line.split('-')
                     try:
                         level = line_array[4].strip()
                     except:
                         raise Exception('Can\'t split {}'.format(line))
-                    counters[level] += 1
-                    if level not in ('INFO', 'DEBUG'):
-                        message.append(line.strip())
-    return message, counters
+                    messages[LOG_TO_NAGIOS_MAPPING[level]].append(line)
+    return messages, counter
 
 
 def parse_arg():
@@ -57,24 +57,28 @@ def main():
     args = parse_arg()
     log_file = args.file
     try:
-        message, counters = load_log(log_file)
+        messages, counter = load_log(log_file)
     except IOError:
-        _exit(NAGIOS_OK, sys.exc_info()[1])
+        _exit(NAGIOS_LEVEL['WARNING'], sys.exc_info()[1])
     else:
-        return_message = '{INFO} info, {ERROR} error, {CRITICAL} critical, out of {All} lines '.format(
-            **counters)
+        messages_length = {key:len(value) for key,value in messages.items()}
+        return_message = '{OK} info, {WARNING} error, {CRITICAL} critical '.format(
+            **messages_length)
+        return_message += 'out of {} lines, '.format(counter)
         return_message += 'from {}. '.format(log_file)
-        if message:
-            return_message += 'Last 4 messages: '
-            return_message += ' '.join(message[-4:])
-            code = NAGIOS_CRITICAL
+        if messages['CRITICAL']:
+            code = NAGIOS_LEVEL['CRITICAL']
+            return_message += ' '.join(messages['CRITICAL'][-4:])
+        elif messages['WARNING']:
+            code = NAGIOS_LEVEL['WARNING']
+            return_message += ' '.join(messages['WARNING'][-4:])
         else:
-            code = NAGIOS_OK
-        return_message += '|All={All} INFO={INFO} WARNING={WARNING} ERROR={ERROR} CRITICAL={CRITICAL}'.format(
-            **counters)
+            code = NAGIOS_LEVEL['OK']
+            return_message += ' '.join(messages['OK'][-2:])
+        return_message += '|OK={OK} WARNING={WARNING} CRITICAL={CRITICAL}'.format(
+            **messages_length)
         _exit(code, return_message)
 
 
 if __name__ == "__main__":
     main()
-
